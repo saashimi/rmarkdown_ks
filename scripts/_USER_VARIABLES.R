d3b4bb13e028655b432d8dd03df8a23438103a83
Kevin Saavedra <saavedrak@metro-region.org>
2017-10-09 13:14:27 -0700

    remove existing datafiles to build from scratch.

--- scripts/_USER_VARIABLES.R
+++
@@ -1,4 +0,0 @@
-# User options to control hardware use, and run mode
-USER_PROCESSOR_CORES <- 1L # How many processors should be used during model runs? (a number, followed by L to indicate integer)
-USER_RUN_MODE <- "Application" # What type of run is being done? Options are "Application" or "Calibration" where calibration triggers certain model components to run iterative adjustments
-
